c849a5a1e560cc698076f66547de84adee229513
Mt Man <mtman1987@gmail.com>
2026-02-02 21:26:25 +0000

    Error: A "use server" file can only export async functions, found object

--- src/firebase/server-init.ts
+++ src/firebase/server-init.ts
@@ -1,14 +1,8 @@
-'use server';
-
 import * as admin from 'firebase-admin';
 import { getFirestore } from 'firebase-admin/firestore';
 import { getAuth } from 'firebase-admin/auth';
 
 if (!admin.apps.length) {
-  // In a Google Cloud environment (like App Hosting), the SDK automatically
-  // finds credentials from the environment. Calling initializeApp() with no
-  // arguments is the correct way to do this. This prevents the SDK from
-  // trying to load local file paths that don't exist in the cloud.
   admin.initializeApp();
 }
 
